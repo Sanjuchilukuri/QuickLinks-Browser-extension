00884eff69cdfadca226100e049069adf71260fc
Sanjuchilukuri <sanjuchilukuri19420@gmail.com>
2025-04-09 01:17:19 +0530

    --commit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
     <div className=" position-absolute bottom-0 start-0 w-100 border border-start-0 border-end-0 border-bottom-0">
         <div className="d-flex justify-content-between mx-3">
             <p className="small text-secondary my-auto">Made with <FaHeart className="text-danger my-auto"/> SANJU</p>
-            <a href="https://www.xnxx.com" target="_blank">Github</a>
+            <a href="https://www.google.com" target="_blank">Github</a>
         </div>
     </div>
   )
